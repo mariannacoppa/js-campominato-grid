b2aeabb4591943988dcb443574b6abcb3076e9fc
mariannacoppa <densimetra@gmail.com>
2024-07-05 17:34:44 +0200

    [UPD] created tag meant to become a single grid square

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // definisco la funzione che crea i quadrati della griglia
 function createSquare () {
-    
+    // creazione del tag html che diventerà il singolo quadrato della griglia
+    let currentElement = document.createElement('div');
 }
