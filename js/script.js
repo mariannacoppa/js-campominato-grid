a881707296326ebaadc02ff6ab6135a7075c5d76
mariannacoppa <densimetra@gmail.com>
2024-07-05 17:35:54 +0200

    [UPD] matched tag with css class

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@
 function createSquare () {
     // creazione del tag html che diventerà il singolo quadrato della griglia
     let currentElement = document.createElement('div');
+    // assegnazione della classe square all'elemento creato
+    currentElement.classList.add('square');
 }
